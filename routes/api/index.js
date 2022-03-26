b2c4bd8add24c4bb097617d4ff618e69dc73cc41
Zack Greenfield <zackg007@verizon.net>
2022-03-26 08:48:59 -0400

    added comment model

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
